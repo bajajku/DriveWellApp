02b0534eb06a03249e3e85bdf3e0455b1ec7942f
Kunal Bajaj <bajajku@sheridancollege.ca>
2023-02-20 14:01:48 -0500

    Added alignment to all labels

--- DriveWellApp/BusinessLogic/CarInventory.cs
+++ DriveWellApp/BusinessLogic/CarInventory.cs
@@ -8,6 +8,38 @@ namespace DriveWellApp.BusinessLogic
 {
     public class CarInventory
     {
+        private List<Car> cars;
+        
 
+        public CarInventory() 
+        
+        {
+            cars = new List<Car>();
+            Car car1 = new Car("1314245678919ABCD", "Mercedes", CarType.Sedan, 40000f, 2020);
+            cars.Add(car1);
+            UsedCar car2 = new UsedCar(1000, "1202920323929FGTD", "BMW", CarType.SUV, 60000, 2021);
+            cars.Add(car2);
+
+        }
+        public Car GetByVin(string vin)
+        {
+            foreach(Car car in cars)
+            {
+                if (car.Vin == vin)
+                    return car;
+            }
+        return null;
+        }
+        public void AddCar(Car car)
+        {
+            if (GetByVin(car.Vin) == null)
+            {
+                cars.Add(car);
+            }
+        }
+        public List<Car> Cars
+        {
+            get { return cars; }
+        }
     }
 }
